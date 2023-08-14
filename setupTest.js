504f5aebbace7085c09a4ef234941797f6906c81
Shahabaz Khan <shahbaz.9676@gmail.com>
2023-08-15 01:15:22 +0530

    Commit: Adding check mark

--- setupTest.js
+++ setupTest.js
@@ -3,3 +3,10 @@ import {configure} from 'enzyme';
 import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
 configure({adapter: new Adapter()});
+
+// const MockInput = () => <View />;
+jest.mock('@rneui/themed', () => ({
+  // AirbnbRating: jest.fn()
+  //   Input: jest.fn(() => <MockInput />),
+  Icon: jest.fn(() => <></>),
+}));
