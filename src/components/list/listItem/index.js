ba1a1baad0bf0ca30dc3c3f9444930ddf9f19bfa
Shahabaz Khan <shahbaz.9676@gmail.com>
2023-08-15 01:39:21 +0530

    Commit: Code smell resolved

--- src/components/list/listItem/index.js
+++ src/components/list/listItem/index.js
@@ -3,24 +3,21 @@ import {Text, TouchableOpacity, View} from 'react-native';
 import {Icon} from '@rneui/themed';
 import styles from './styles';
 
-const ListItem = ({item, updateItem}) => {
-  const iconName = item?.done ? 'check-box' : 'check-box-outline-blank';
-  return (
-    <View>
-      <View style={styles.listItem}>
-        <TouchableOpacity
-          onPress={() => {
-            updateItem({...item, done: !item?.done});
-          }}>
-          <Icon name={item?.done ? 'check-box' : 'check-box-outline-blank'} />
-        </TouchableOpacity>
-        <Text style={item?.done ? styles.textDone : styles.text}>
-          {item?.value}
-        </Text>
-      </View>
-      <View style={styles.seperator} />
+const ListItem = ({item, updateItem}) => (
+  <View>
+    <View style={styles.listItem}>
+      <TouchableOpacity
+        onPress={() => {
+          updateItem({...item, done: !item?.done});
+        }}>
+        <Icon name={item?.done ? 'check-box' : 'check-box-outline-blank'} />
+      </TouchableOpacity>
+      <Text style={item?.done ? styles.textDone : styles.text}>
+        {item?.value}
+      </Text>
     </View>
-  );
-};
+    <View style={styles.seperator} />
+  </View>
+);
 
 export default ListItem;
