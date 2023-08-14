3896938afe3be82fecb53d4ca921286650834fcb
Shahabaz Khan <shahbaz.9676@gmail.com>
2023-08-14 23:18:35 +0530

    Commit: Using adapter 18

--- setupTest.js
+++ setupTest.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import {configure} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
 configure({adapter: new Adapter()});
